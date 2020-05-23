8d3151907a26025d2eec957809cda7f207109afa
Harshal Mehta <harshalwork157@gmail.com>
2020-05-23 16:59:38 +0530

    Folder compatibility added, but not compatible for duplicate files

--- fs.py
+++ fs.py
@@ -1,35 +1,41 @@
 import os
 import shutil
 import time
-st= time.clock()
+st= time.clock()        #starttime
 #cwd= os.getcwd()
 #print(cwd)
-path="C:\\Users\\harsh\\Desktop\\Test"
-dpath= os.listdir(path)     #list containing names of all files in the path in the form of strings
-#print(dpath)
-file=[os.path.splitext(i)    for i in dpath]        #list containing a list of tuples with the name and extensions as elements
+path="C:\\Users\\harsh\\Desktop\\Test"      #path in which files to be sorted are present
+fpath="C:\\Users\\harsh\\Desktop\\Test"    #path in which files are to be sorted
+
+if os.path.exists(fpath) is False:      #incase if the final destinaton folder doesnt exist
+    os.mkdir(fpath)
+
+dpath= os.listdir(path)     #list containing names of all files in the sourcepath in the form of strings
+#file=[]
+#print(len(dpath))
+for i in dpath:
+    if os.path.isdir(path+"\\"+i):      #to avoid folders
+        dpath.remove(i)             #remove all folders from the path
+
+file=[os.path.splitext(i)    for i in dpath] #if os.path.splitext(i)[1]!=""]       #list containing a list of tuples with the name and extensions as elements
+
 ext=[]
-
 for i in file[:]:
-    ext.append(i[1])        #creating a list with extensions
+    ext.append(i[1])        #creating a list with extensions (there can be similar elements)
 
-unique_ext= list(set(ext))      #converting ext into a list which contains unique extensions only
+unique_ext= list(set(ext))      #converting ext into a list which contains unique elements only
+k=len(unique_ext)
 
-for i in unique_ext[:]:
-    n_path= os.path.join(path,((i[1:].capitalize())+" Files"))      #create a folder with the name of extension
-    if os.path.exists(n_path) is False:
-        os.mkdir(n_path)
+for i in range(len(file)+len(unique_ext)):      #looping through to create extension folders and also move the files respectively in the same loop
+    if k!=0:
+        k-=1
+        n_path= os.path.join(fpath,((unique_ext[i][1:].capitalize())+" Files"))      #create a folder with the name of extension
+        if os.path.exists(n_path) is False:     #incase if folder doesnt exist, create it
+            os.mkdir(n_path)
+    else:
+        source= os.path.join(path,dpath[i-len(unique_ext)])     #selecting the file in the source folder
+        dest= os.path.join(fpath,((file[i-len(unique_ext)][1])[1:].capitalize()+" Files"))      #selecting the folder in which the file is to be moved
+        shutil.move(source,dest)    #moving the files
 
-for i in range(len(file)):
-    source= os.path.join(path,dpath[i])
-    dest= os.path.join(path,((file[i][1])[1:].capitalize()+" Files"))
-    #print("Source: ",source,"Destination: ",dest)
-    #print(dest)
-    #if os.path.exists(n_path) is False:
-    #    os.mkdir(dest)
-    #    shutil.move(source,dest)
-    #else:
-    shutil.move(source,dest)
-
-et=time.clock()
+et=time.clock()     #end time
 print(et-st)
